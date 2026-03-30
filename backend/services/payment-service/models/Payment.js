703221de46d47399dc843a90e59f07efcac716eb
Yuhansi <it23245310@my.sliit.lk>
2026-03-31 02:43:07 +0530

    Create Payment model schema

---
+++ backend/services/payment-service/models/Payment.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+  userId: String,
+  appointmentId: String,
+  amount: Number,
+  status: {
+    type: String,
+    default: "pending",
+  },
+  stripeSessionId: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Payment", paymentSchema);
+
+
+
