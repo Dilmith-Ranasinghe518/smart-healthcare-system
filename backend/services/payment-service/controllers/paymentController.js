279ff3f0aec2bd2ed69073ad7f997856bf45bdca
Yuhansi <it23245310@my.sliit.lk>
2026-03-31 02:42:19 +0530

    Create payment controller logic for handling payment requests

---
+++ backend/services/payment-service/controllers/paymentController.js
@@ -0,0 +1,107 @@
+const Stripe = require("stripe");
+const Payment = require("../models/Payment");
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+exports.createPayment = async (req, res) => {
+  try {
+    const { userId, appointmentId, amount,currency, title } = req.body;
+
+    const session = await stripe.checkout.sessions.create({ 
+      payment_method_types: ["card"],
+      mode: "payment", 
+      line_items: [ 
+        { 
+        price_data: { currency: currency || "usd", 
+        product_data: { name: title || "Doctor Consultation", 
+        }, 
+        unit_amount: amount, 
+      }, quantity: 1, 
+    }, ],
+      success_url: "http://localhost:3000/payment/success?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: "http://localhost:3000/payment/cancel",
+    });
+
+    const payment = new Payment({
+      userId,
+      appointmentId,
+      amount,
+      status: "pending",
+      stripeSessionId: session.id,
+    });
+
+    await payment.save();
+
+    res.json({ url: session.url });
+
+  } catch (error) {
+    res.status(500).json({ message: "Payment error" });
+  }
+};
+
+exports.getAllPayments = async (req, res) => {
+  try {
+    const payments = await Payment.find();
+    res.json(payments);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.markAsPaid = async (req, res) => {
+  try {
+    const payment = await Payment.findOne({
+      stripeSessionId: req.params.sessionId,
+    });
+
+    if (payment) {
+      payment.status = "paid";
+      await payment.save();
+
+      res.json({ message: "Payment marked as PAID" });
+    } else {
+      res.status(404).json({ message: "Payment not found" });
+    }
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.refundPayment = async (req, res) => {
+  try {
+    const payment = await Payment.findById(req.params.id);
+
+    if (!payment) {
+      return res.status(404).json({ message: "Payment not found" });
+    }
+
+    payment.status = "refunded";
+    await payment.save();
+
+    res.json({ message: "Payment refunded successfully" });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getFinancialSummary = async (req, res) => {
+  try {
+    const payments = await Payment.find({ status: "paid" });
+
+    const totalRevenue = payments.reduce(
+      (sum, p) => sum + p.amount,
+      0
+    );
+
+    res.json({
+      totalRevenue,
+      totalTransactions: payments.length,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
